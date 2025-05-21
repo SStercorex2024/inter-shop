c9befaa97cdc8bdfe76bff555e4387edb6964aaf
stercorexze <stercorexze@gmail.com>
2025-05-21 22:46:52 +0200

    added section accessories, finished products page

--- app/js/components/btnChangeColor.js
+++
@@ -1,7 +0,0 @@
-export default function btnChangeColor() {
-  document.querySelectorAll(".collections__like").forEach((button) => {
-    button.addEventListener("click", () => {
-      button.classList.toggle("collections__like-active");
-    });
-  });
-}
--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,43 @@
-import sanitizeForm from "./components/formSanitizer.js";
+const swiper = new Swiper(".accessories__slider", {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  loop: true,
+  navigation: {
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-prev",
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  }
+});
 
-sanitizeForm(".join-us__form");
+// санітайзер
+function initFormAndButtons() {
+  function sanitizeInput(str) {
+    return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  }
 
-import btnChangeColor from "./components/btnChangeColor.js";
+  // обробка форми
+  const form = document.querySelector("form");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      const inputs = form.querySelectorAll("input");
+      inputs.forEach((input) => {
+        input.value = sanitizeInput(input.value);
+      });
+    });
+  }
 
-btnChangeColor();
+  // обробка кнопки лайку
+  function btnChangeColor() {
+    document.querySelectorAll(".collections__like").forEach((button) => {
+      button.addEventListener("click", () => {
+        button.classList.toggle("collections__like-active");
+      });
+    });
+  }
+  btnChangeColor();
+}
 
-import swiperForProducts from "./components/swiperForProducts.js";
-
-swiperForProducts();
+initFormAndButtons();
