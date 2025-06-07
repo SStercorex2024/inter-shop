3d9b17eed1d47ba135958b4c668329f0034e2513
stercorexze <stercorexze@gmail.com>
2025-06-07 18:33:54 +0200

    added page review

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,11 @@
 //= include modules/swiper.js
+//= include modules/swiperCustomer.js
 //= include modules/sanitizeInput.js
 //= include modules/likeBtn.js
 //= include modules/linkActive.js
 
-
+if (typeof swiperCustomer === "function") swiperCustomer();
 if (typeof swiperInit === "function") swiperInit();
 if (typeof initSanitize === "function") initSanitize();
 if (typeof likeBtn === "function") likeBtn();
 if (typeof linkActive === "function") linkActive();
-
